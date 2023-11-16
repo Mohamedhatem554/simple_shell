fe86726e31409e1df3a3791ba0853e3ee9a0e007
Dark Weirdo <sonic_unleashed@live.com>
2023-11-17 00:20:51 +0200

    betty + description fix

--- execute.c
+++ execute.c
@@ -2,31 +2,31 @@
 
 /**
  * _execute - execute function
- * @cmd: command
- * @argv: argv
+ * @cmd: command index
+ * @argv: argv vector
  * Return: status
 */
 int _execute(char **cmd, char **argv)
 {
-	pid_t cpid;
-	int stat;
+pid_t cpid;
+int stat;
 
-	cpid = fork();
-	if (cpid == 0)
-	{
-		if (execve(cmd[0], cmd, environ) == -1)
-		{
-			perror(argv[0]);
-			freearr(cmd);
-			exit(0);
-		}
+cpid = fork();
+if (cpid == 0)
+{
+if (execve(cmd[0], cmd, environ) == -1)
+{
+perror(argv[0]);
+freearr(cmd);
+exit(0);
+}
 
-	}
-	else
-	{
-		waitpid(cpid, &stat, 0);
-		freearr(cmd);
-	}
-	return (WEXITSTATUS(stat));
+}
+else
+{
+waitpid(cpid, &stat, 0);
+freearr(cmd);
+}
+return (WEXITSTATUS(stat));
 
 }
