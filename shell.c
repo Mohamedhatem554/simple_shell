143e6a7672b8a29a9e07449d940a6c9c9b166263
Dark Weirdo <sonic_unleashed@live.com>
2023-11-17 00:22:59 +0200

    betty + description fix

--- shell.c
+++ shell.c
@@ -2,33 +2,33 @@
 
 /**
  * main - main function
- * @argc: argc
- * @argv: argv
+ * @argc: argument index
+ * @argv: arg vector
  * @env: env
  * Return: line
 */
 int main(int argc, char **argv, char **env)
 {
-	char *line = NULL, **cmd = NULL;
-	int stat = 0;
+char *line = NULL, **cmd = NULL;
+int stat = 0;
 
-	(void)argc;
-	(void)env;
+(void)argc;
+(void)env;
 
-	while (1)
-	{
-		line = _getline();
-		if (line == NULL)
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			return (stat);
-		}
-		cmd = taketoken(line);
+while (1)
+{
+line = _getline();
+if (line == NULL)
+{
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "\n", 1);
+return (stat);
+}
+cmd = taketoken(line);
 
-		if (!cmd)
-		continue;
-		free(line);
-		stat = _execute(cmd, argv);
-	}
+if (!cmd)
+continue;
+free(line);
+stat = _execute(cmd, argv);
+}
 }
